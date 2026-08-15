f29836bd9acdae4a56df0fc12aeab2ec395fa125
Rohan7207 <rohanfigredo@gmail.com>
2026-08-15 17:27:23 +0530

    Creating home page for user

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      {/* This is sample data */}
+
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full ">
+          <i className="ri-map-pin-2-fill"></i>
+        </h2>
+        <h4 className="font-medium">
+          24B, Near Kapoor's cafe, Shreyians Coding School, Bhopal
+        </h4>
+      </div>
+
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full ">
+          <i className="ri-map-pin-2-fill"></i>
+        </h2>
+        <h4 className="font-medium">
+          24B, Near Kapoor's cafe, Shreyians Coding School, Bhopal
+        </h4>
+      </div>
+
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full ">
+          <i className="ri-map-pin-2-fill"></i>
+        </h2>
+        <h4 className="font-medium">
+          24B, Near Kapoor's cafe, Shreyians Coding School, Bhopal
+        </h4>
+      </div>
+
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full ">
+          <i className="ri-map-pin-2-fill"></i>
+        </h2>
+        <h4 className="font-medium">
+          24B, Near Kapoor's cafe, Shreyians Coding School, Bhopal
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React, { useRef, useState } from "react";
 import homeLogo from "../assets/map_image.gif";
 import { useGSAP } from "@gsap/react"; //  GSAP is animation hook
 import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
+import LocationSearchPanel from "../components/LocationSearchPanel";
+import carImage from "../assets/car_png.jpg";
 
 const Home = () => {
   const [pickUp, setPickUp] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -18,10 +22,18 @@ const Home = () => {
       if (panelOpen) {
         gsap.to(panelRef.current, {
           height: "70%",
+          padding: 24,
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 1,
         });
       } else {
         gsap.to(panelRef.current, {
           height: "0%",
+          padding: 0,
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 0,
         });
       }
     },
@@ -29,7 +41,7 @@ const Home = () => {
   );
 
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       <img
         className="w-16 absolute left-5  top-5"
         src={homeLogo}
@@ -43,6 +55,15 @@ const Home = () => {
 
       <div className=" flex flex-col justify-end h-screen absolute top-0 w-full ">
         <div className="h-[30%] bg-white p-6 relative">
+          <h5
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
+            }}
+            className="absolute opacity-0 right-6 top-6 text-2xl"
+          >
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form
             onSubmit={(e) => {
@@ -77,7 +98,26 @@ const Home = () => {
           </form>
         </div>
 
-        <div ref={panelRef} className="h-0 bg-red-500"></div>
+        <div ref={panelRef} className="h-0 bg-white">
+          {<LocationSearchPanel />}
+        </div>
+      </div>
+
+      <div className="fixed z-10 bottom-0">
+        <div>
+          <img src={carImage} alt="Car Image" />
+          <div>
+            <h4>
+              UberGo{" "}
+              <span>
+                <i className="ri-user-3-fill">4</i>
+              </span>
+            </h4>
+            <h5>2 mins away </h5>
+            <p>Affordable, compact rides</p>
+          </div>
+          <h2></h2>
+        </div>
       </div>
     </div>
   );
